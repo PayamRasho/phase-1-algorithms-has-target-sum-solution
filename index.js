bd14f12ebc67856fa35beb55f62a07de8b185117
PayamRasho <payamrfattah01@gmail.com>
2024-03-10 20:52:26 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
+    for (let i = 0; i < array.length; i++) {
+      const complement = target - array[i];
+      for (let j = i + 1; j < array.length; j++) {
+        if (array[j] === complement) return true;
+      }
+    }
+    return false;
+  }
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,6 +16,13 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  iterate over the remaining numbers in the array
+    check if any of the remaining numbers is the complement
+      if so, return true
+if we reach the end of the array, return false
 */
 
 /*
